452809724ca7a260376b4eb0ea367635e665c49d
rohit kumar <12rohit4@gmail.com>
2018-03-04 05:13:41 +0530

    http get request using curl. cpp doesnot have a built in http library so using curl.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
 
+#include <iostream>
+#include <stdio.h>
+#include "curl/curl.h"
 using namespace std;
 
-int main()
+int main(void)
 {
-    cout << "Hello lets start a youtube downloader project" << endl;
+  CURL *curl;
+  CURLcode res;
 
-    return 0;
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://youtube.com");
+    /* example.com is redirected, so we tell libcurl to follow redirection */
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return 0;
 }
